9ba70e418730528b7465bed4438f794813ea5938
ransomware <4thel00z@gmail.com>
2017-09-17 23:30:50 +0200

    Add appropiate parser for the unfollower

--- app.py
+++ app.py
@@ -6,29 +6,43 @@ from api.unfollower import Unfollower
 from utils.auth import get_authenticated_api
 
 
+#
+#   Parser handler section
+#
 
+
+def twitter_handler(args):
+    api = get_authenticated_api()
+    unfollower = Unfollower(api)
+    unfollower.destroyAllFriendships(only_non_follower=args.only_non_followers)
+
+
+
+#
+#   Parser setup section
+#
 
 parser = argparse.ArgumentParser()
 
 subParsers = parser.add_subparsers(help='[command] help')
 
-
 twitterParser = subParsers.add_parser("twitter",
                                       help="invokes the twitter related commands")
-
 twitterParser.add_argument_group()
-
+twitterParser.set_defaults(func=twitter_handler)
 twitterParser.add_argument("-n", "--only-non-followers", default=False, action="store_true")
 
+twitterParser.add_argument("-p", "--print-on-delete", help="Prints the names of deleted users", default=False,
+                           action="store_true")
 
-twitterParser.add_argument("-p", "--print-on-delete", help="Prints the names of deleted users",default=False, action="store_true")
 
-
+#
+#   Main method section
+#
 
 def main():
-    api = get_authenticated_api()
-    unfollower = Unfollower(api)
-    unfollower.destroyAllFriendships()
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == '__main__':
